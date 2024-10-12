1aab4f262da6ed61c232b1feb407358a7af860ae
Yokio9 <dmatayi@gmail.com>
2024-10-12 12:56:05 +0200

    can recognize different types

--- ft_printf.c
+++ ft_printf.c
@@ -1,32 +1,60 @@
 #include <stdarg.h>
 #include "../libft/libft.h"
 
+static void	type_to_print(char *dup_format)
+{
+	char	*num;
+	char	*character;
+	char	*hexa;
+
+	num = ft_strdup("diu");
+	character = ft_strdup("cs%");
+	hexa = ft_strdup("pXx");
+	while (*num)
+		if (*dup_format == *num++)
+			ft_putendl_fd("found a num", 1);
+	while (*character)
+		if (*dup_format == *character++)
+			ft_putendl_fd("found a character", 1);
+	while (*hexa)
+		if (*dup_format == *hexa++)
+			ft_putendl_fd("found an hexa", 1);
+}
+
+static int	count_args(char *format)
+{
+	int	num_args;
+	char *dup_format;
+
+	num_args = 0;
+	dup_format = ft_strdup(format);
+	while (dup_format)
+	{
+		dup_format = ft_strchr(dup_format, '%');
+		if (!dup_format)
+			break;
+		num_args++;
+		dup_format++;
+		type_to_print(dup_format);
+		if (*dup_format == '%')
+			dup_format++;
+	}
+	return (num_args);
+}
+
 void	ft_printf(char *format, ...)
 {
 	va_list	args;
-	int	i;
-	int	j;
-	int	num_args;
-	char *char_ptr;
+	int		i;
+	int		j;
 
-	i = 0;
+	i = count_args(format);
 	j = 0;
-	num_args = 0;
-	char_ptr = ft_strdup(format);
-	while (char_ptr)
-	{
-		char_ptr = ft_strchr(char_ptr, '%');
-		if (!char_ptr)
-			break;
-		num_args++;
-		char_ptr++;
-	}
 	va_start(args, format);
-	while (i < num_args)
+	while (i-- > 0)
 	{
 		j = va_arg(args, int);
 		ft_putendl_fd(ft_itoa(j), 1);
-		i++;
 	}
 	va_end(args);
 }
@@ -36,6 +64,6 @@ int main()
 	int	one = 23;
 	int	two = 6;
 	int	three = 9;
-	ft_printf("%ladbah%adjanb%aefea", one, two, three);
+	ft_printf("%dla%iaj%uef%ckjb%slkb%%bu%pkljbh%Xoizvb%xdf", one, two, three, one, two, three, one, two, three);
 	return 0;
 }
